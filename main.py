078e26a41b188a3804b868a8910d83ed93b881cf
Marcondes <marcondesgoncalves159@gmail.com>
2024-04-05 18:00:45 -0300

    CSRF TOKEN e Mensagens de Erro ou Sucesso nos Formulários

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 
 # url_for está sendo utilizado nos links das páginas HTML, o mesmo referência as funções referentes ao seu ROUTE. 
@@ -25,10 +25,23 @@ def usuarios() -> str:
     return render_template("usuarios.html", lista_usuarios = lista_usuarios)
 
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login() -> str:
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        #Exibir msg de login bem sucedido
+        flash(f"Login feito com sucesso no E-mail: {form_login.email.data}", 'alert-success')
+        #Redirecionar para homepage
+        return redirect(url_for('home'))
+    
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        #Exibir msg de conta criada com sucesso
+        flash(f"Conta criada com sucesso no E-mail: {form_criarconta.email.data}", 'alert-success')
+        #Redirecionar para homepage
+        return redirect(url_for('home'))
+    
     return render_template("login.html", form_login = form_login, form_criarconta = form_criarconta)
 
 
