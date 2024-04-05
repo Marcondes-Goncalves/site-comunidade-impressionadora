8a52187097af929fdf78d1a1a6ec5850a821ade6
Marcondes <marcondesgoncalves159@gmail.com>
2024-04-05 17:01:28 -0300

    Aplicando BOOTSTRAP nos formulários

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import  DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
@@ -8,7 +8,7 @@ class FormCriarConta(FlaskForm):
     username: str = StringField("Nome do Usuário", validators = [DataRequired()])
     email: str = StringField("E-mail", validators = [DataRequired(), Email()])
     senha: str = PasswordField("Senha", validators = [DataRequired(), Length(6, 20)])
-    confirmacao = PasswordField("Confirmação da senha", validators = [DataRequired(), EqualTo("senha")])
+    confirmacao = PasswordField("Confirmação de senha", validators = [DataRequired(), EqualTo("senha")])
     botao_submit_criarconta = SubmitField("Criar Conta")
 
 
@@ -16,4 +16,6 @@ class FormLogin(FlaskForm):
 
     email: str = StringField("E-mail", validators = [DataRequired(), Email()])
     senha: str = PasswordField("Senha", validators = [DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField("Lembrar dados de acesso")
     botao_submit_login = SubmitField("Fazer Login")
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
 from flask import Flask, render_template, url_for
+from forms import FormCriarConta, FormLogin
 
 # url_for está sendo utilizado nos links das páginas HTML, o mesmo referência as funções referentes ao seu ROUTE. 
 
 app = Flask(__name__)
 
 lista_usuarios: list[str] = ["Marcondes", "Maria", "Ana", "João", "Rosa"]
+
+app.config["SECRET_KEY"] = '8c5762d6223bc0970d3936c2c0deb095'
 
 @app.route("/")
 def home() -> str:
@@ -24,7 +27,9 @@ def usuarios() -> str:
 
 @app.route("/login")
 def login() -> str:
-    return render_template("login.html")
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template("login.html", form_login = form_login, form_criarconta = form_criarconta)
 
 
 
