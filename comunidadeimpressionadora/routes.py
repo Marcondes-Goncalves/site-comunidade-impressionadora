c3abeea7d22cca68eee891d38cf10431e2281862
Marcondes <marcondesgoncalves159@gmail.com>
2024-04-08 17:50:19 -0300

    Criptografando a senha do usuário

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -2,8 +2,10 @@
 from flask import flash, render_template, redirect, url_for, request
 
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
-from comunidadeimpressionadora import app
+from comunidadeimpressionadora import app, database, bcrypt
+from comunidadeimpressionadora.models import Usuario
 
+# url_for está sendo utilizado nos links das páginas HTML, o mesmo referência as funções referentes ao seu ROUTE.
 
 lista_usuarios: list[str] = ["Marcondes", "Maria", "Ana", "João", "Rosa"]
 
@@ -34,6 +36,14 @@ def login() -> str:
         return redirect(url_for('home'))
     
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        #Criptografa a senha
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        #Criar o usuário
+        usuario = Usuario(username = form_criarconta.username.data, email = form_criarconta.email.data, senha = senha_cript)
+        #Adicionar a sessão
+        database.session.add(usuario)
+        #Commit na sessão
+        database.session.commit()
         #Exibir msg de conta criada com sucesso
         flash(f"Conta criada com sucesso no E-mail: {form_criarconta.email.data}", 'alert-success')
         #Redirecionar para homepage
